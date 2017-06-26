9d35147f849ed02a9efa410fce3aacd250435c9d
David Ellis <davidkellis@gmail.com>
2017-06-25 22:55:48 -0500

    hackjob program works!

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	"github.com/BurntSushi/toml"
 	"github.com/labstack/echo"
 )
 
 // var driver *agouti.WebDriver = nil
+var configFile = flag.String("config", "config.toml", "TOML formatted config file path")
 var server *echo.Echo = nil
 
 func handleCtrlC(c chan os.Signal) {
@@ -19,49 +23,55 @@ func handleCtrlC(c chan os.Signal) {
 
 	// handle ctrl+c event here
 	fmt.Println("\nsignal: ", sig)
-	// if driver != nil {
-	// 	driver.Stop()
-	// }
 	if server != nil {
 		server.Shutdown(time.Second * 1)
 	}
 	os.Exit(0)
 }
 
+type Config struct {
+	ApiKeys []string
+	Routes map[string]map[string]Route
+}
+
+type Route struct {
+	URL string
+	User string
+	Pass string
+}
+
 func main() {
+	flag.Parse()
+
+	var config Config
+	_, err := toml.DecodeFile(*configFile, &config)
+	if err != nil {
+		log.Fatalf("Failed to read config file %v.", *configFile)
+	}
+
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, os.Kill)
 	go handleCtrlC(c)
 
-	//driver = agouti.Selenium()
-	//capabilities := agouti.NewCapabilities().Browser("firefox")
-	//page, err := driver.NewPage(agouti.Desired(capabilities))
-	// driver = agouti.Selenium()
-	// capabilities := agouti.NewCapabilities().Browser("firefox")
-	// page, err := driver.NewPage(agouti.Desired(capabilities))
-	// if err != nil {
-	// 	fmt.Errorf("Unable to ")
-	// }
-	// page.Navigate("http://www.drudgereport.com")
-	// page.Screenshot("~/Desktop/drudge.jpg")
-
 	server := echo.New()
 
 	knownKeys := make(map[string]bool)
-	knownKeys["55555"] = true		// key 55555 is enabled
+	for _, key := range config.ApiKeys {
+		knownKeys[key] = true		// enable all keys initially
+	}
 
 	// client supplies the following URL-encoded query params:
 	//   url (required) - the URL of the target resource
 	//   user (optional) - basic auth username to authenticate against <url>
 	//   pass (optional) - basic auth password to authenticate against <url>
-	server.GET("/:key/raw", func(c echo.Context) error {
+	server.GET("/:key/get", func(c echo.Context) error {
 		key := c.Param("key")
 		url := c.QueryParam("url")
 		username := c.QueryParam("user")
 		password := c.QueryParam("pass")
 
 		if keyEnabled, present := knownKeys[key]; !present || !keyEnabled {
-			fmt.Println("Access key not allowed: %v", key)
+			fmt.Printf("Access key not allowed: %v", key)
 			return c.String(http.StatusUnauthorized, "Access key not allowed.")
 		}
 
@@ -91,21 +101,56 @@ func main() {
 		return c.Blob(res.StatusCode, contentType, bodyByteSlice)
 	})
 
-	server.GET("/:key/frontdoor", func(c echo.Context) error {
-		return c.String(http.StatusOK, "render frontdoor")
-	})
+	server.GET("/:key/:route", func(c echo.Context) error {
+		key := c.Param("key")
+		routeName := c.Param("route")
 
-	server.GET("/:key/garage", func(c echo.Context) error {
-		return c.String(http.StatusOK, "render backdoor")
-	})
+		if keyEnabled, present := knownKeys[key]; !present || !keyEnabled {
+			fmt.Printf("Access key not allowed: %v", key)
+			return c.String(http.StatusUnauthorized, "Access key not allowed.")
+		}
 
-	server.GET("/:key/backdoor", func(c echo.Context) error {
-		return c.String(http.StatusOK, "render backdoor")
+		routes, present := config.Routes[key]
+		if !present {
+			fmt.Println("Unknown route.")
+			return c.String(http.StatusNotFound, "Unknown route.")
+		}
+
+		route, present := routes[routeName]
+		if !present {
+			fmt.Println("Unknown route.")
+			return c.String(http.StatusNotFound, "Unknown route.")
+		}
+
+		url := route.URL
+		username := route.User
+		password := route.Pass
+
+		// issue GET request
+		client := http.Client{}
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to create request.")
+		}
+
+		if username != "" && password != "" {
+			req.SetBasicAuth(username, password)
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to submit request.")
+		}
+
+		// process response
+		contentType := res.Header.Get("Content-Type")
+		bodyByteSlice, err := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to read response from remote host.")
+		}
+		return c.Blob(res.StatusCode, contentType, bodyByteSlice)
 	})
 
 	server.Logger.Fatal(server.Start(":4444"))
-}
-
-func get(url, username, password string) (*http.Response, error) {
-	return nil, nil
 }
